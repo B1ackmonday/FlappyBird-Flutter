63fcf02814d5c7427d2008f0eb9485525ef1db13
B1ackmonday <94772596+B1ackmonday@users.noreply.github.com>
2021-12-21 18:01:24 +0700

    Add files via upload

---
+++ flappybird/lib/barriers.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class MyBarrier extends StatelessWidget {
+  final size;
+
+  MyBarrier({this.size});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      height: size,
+      decoration: BoxDecoration(
+          color: Colors.green,
+          border: Border.all(width: 10, color: Colors.grey),
+          borderRadius: BorderRadius.circular(15)),
+    );
+  }
+}
